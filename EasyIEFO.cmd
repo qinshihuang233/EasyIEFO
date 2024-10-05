4a19318c878fbb1660c81c0d1c8b78134e572ed0
qinshihuang233 <sswslr58@outlook.com>
2024-10-05 22:52:14 +0800

    修复了若干Bug

--- EasyIEFO.cmd
+++ EasyIEFO.cmd
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErTXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbQs=
+::ZQ05rAF9IAHYFVzEqQIVKQhNZSGkEws=
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQK08PvSgNoH06Q+3XCUJiF8GyIBSpqViTm5hVFcBkIG/hEk40vgeoE+UIKaHSjjifGT+P5K6fXQnVtrM7RqshBZNuo2Kg==
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDpQQQ2MNXiuFLQI5/rHy++UqVkSRN4aeZzC85GoD9xBpED8cPY=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 echo EasyIEFO
 echo   -version 1.0.0 By  Bilibili @sheep_003 ^& 抖音 @sheep_003 ^(UID: 41145878797^)
@@ -37,7 +70,7 @@ if /i %mode% == R (
 
 
 :finish
-pause>按任意键继续操作......
+pause
 goto start
 
 rem ------------------------------------------------------------------------------------
